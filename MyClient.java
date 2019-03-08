32d3658e3ad9efa605e63cb30a3e2d0026658392
Colin <Colin.j.halter@gmail.com>
2019-03-08 09:18:22 -0500

    Added initial client code.

--- MyClient.java
+++ MyClient.java
@@ -1,5 +1,7 @@
 import java.net.*;
 import java.awt.*;
+import java.awt.event.*;
+
 import javax.swing.*;
 
 import javax.swing.JFrame;
@@ -9,50 +11,64 @@ import java.io.*;
 /* SHOULD ONLY COMMIT TO GUI
 */
 
-public class MyClient
+public class MyClient extends Thread implements ActionListener
 {
-	public MyClient() {
-		JFrame mainFrame = new JFrame();
-		JPanel jpSouth = new JPanel();
-		JButton jbSend = new JButton("SEND");
-		JTextArea jtaMessage = new JTextArea("Please enter sever IP in message box below.");
-		JScrollPane jsaScroll = new JScrollPane(jtaMessage);
-		JTextField jtfMessageInput = new JTextField();
-		//jtfMessageInput.setSize(10, 100);
-		jpSouth.setLayout(new BorderLayout());
-		mainFrame.add(jpSouth, BorderLayout.SOUTH);
-		mainFrame.add(jsaScroll, BorderLayout.CENTER);
-		jpSouth.add(jbSend, BorderLayout.CENTER);
-		jpSouth.add(jtfMessageInput, BorderLayout.NORTH);
-		jtaMessage.setSize(300, 300);
-		jtaMessage.setEditable(false);
-		jsaScroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-		jsaScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		mainFrame.setSize(500, 500);
-		mainFrame.setVisible(true);
-		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	private static Socket s;
+	private JTextArea jtaMessage;
+	private JTextField jtfMessageInput;
+	private static String ip;
+	
+	public MyClient()
+	{
+		try
+		{	
+			JFrame mainFrame = new JFrame();
+			JPanel jpSouth = new JPanel();
+			JButton jbSend = new JButton("SEND");
+			jtaMessage = new JTextArea("Please enter sever IP in message box below.");
+			JScrollPane jsaScroll = new JScrollPane(jtaMessage);
+			jtfMessageInput = new JTextField();
+			//jtfMessageInput.setSize(10, 100);
+			jpSouth.setLayout(new BorderLayout());
+			mainFrame.add(jpSouth, BorderLayout.SOUTH);
+			mainFrame.add(jsaScroll, BorderLayout.CENTER);
+			jpSouth.add(jbSend, BorderLayout.CENTER);
+			jpSouth.add(jtfMessageInput, BorderLayout.NORTH);
+			jtaMessage.setSize(300, 300);
+			jtaMessage.setEditable(false);
+			jsaScroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+			jsaScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+			mainFrame.setSize(500, 500);
+			mainFrame.setVisible(true);
+			mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+			jbSend.addActionListener(this);
+			
+			s = new Socket("192.168.0.125", 16789);
+			System.out.println("Connected to " + s.getInetAddress());
+		} 
+		catch (UnknownHostException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public static void main(String [] args)
 	{
 		try{
-			
+			ip = args[0];
 			new MyClient();
 			System.out.println("getLocalHost: "+InetAddress.getLocalHost() );
 			System.out.println("getByName:    "+InetAddress.getByName("localhost") );
-			Socket s = new Socket( args[0], 16789);
-			InputStream in = s.getInputStream();
-			OutputStream out = s.getOutputStream();
+			s = new Socket( args[0], 16789);
 			
-			PrintWriter pout = new PrintWriter(out);
-			pout.println(args[1]);		// Writes some String to server
-			pout.flush(); 					// forces the data through to server
-			
-			BufferedReader bin = new BufferedReader(new InputStreamReader(in));
-			System.out.println(args[1]+" <=returned-as=> " + bin.readLine());
-
-			out.close();
-			pout.close();
-			s.close();
+			Thread serverLoop = new Thread();
+			serverLoop.start();
 		}
 		catch(UnknownHostException uhe) {
 			System.out.println("no host");
@@ -66,5 +82,42 @@ public class MyClient
 	   catch( ArrayIndexOutOfBoundsException aioobe ) {
 	 		System.out.println("\nUsage: java Day10Server hostname some-word");
 	 	}
+	}
+	
+	public synchronized void actionPerformed(ActionEvent ae)
+	{
+		try
+		{
+
+			OutputStream out = s.getOutputStream();
+			String temp = jtfMessageInput.getText();
+			PrintWriter pout = new PrintWriter(out);
+			pout.println(temp);		// Writes some String to server
+			jtaMessage.setText(jtaMessage.getText()+ "\n" + temp);
+			pout.flush(); 					// forces the data through to server
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void run()
+	{
+		try
+		{
+			InputStream in = s.getInputStream();
+			BufferedReader bin = new BufferedReader(new InputStreamReader(in));
+			
+			while(true)
+			{
+				jtaMessage.setText(jtaMessage.getText() + "\n" + bin.readLine());
+			}
+			
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
